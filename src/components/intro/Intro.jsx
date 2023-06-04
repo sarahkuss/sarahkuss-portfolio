c31d6c8fb619b920d22c1f3fe870f458d34ec1e1
Sarah Kuss <sarah.kuss33@gmail.com>
2023-06-03 22:50:49 -0400

    first commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function Intro () {
+  return (
+    <main>
+      <Container>
+        <Row>
+
+        <Col></Col>
+        <Col>
+          <h1>Hi! I'm Sarah</h1>
+        </Col>
+        </Row>
+      </Container>
+    </main>
+  )
+}
