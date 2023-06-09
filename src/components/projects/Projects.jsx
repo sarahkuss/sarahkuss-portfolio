e42543b6e3f41dde1e301f4d4e64f382aff4e14e
Sarah Kuss <sarah.kuss33@gmail.com>
2023-06-08 20:01:23 -0400

    added more files

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,13 @@
+import { Card, Container, Row } from "react-bootstrap";
+
+export default function Projects () {
+  return(
+    <>
+    <Container>
+      <Row>
+        <Card></Card>
+      </Row>
+    </Container>
+    </>
+  )
+}
