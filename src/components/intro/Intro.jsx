9c6229f11e984d52082e48a5e8ce7c53c154f0ee
Sarah Kuss <sarah.kuss33@gmail.com>
2023-06-20 21:20:42 -0400

    added my photo

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,14 +1,16 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 export default function Intro () {
   return (
     <main>
-      <Container>
-        <Row>
-        <Col></Col>
-        <Col>
-          <h1>Hi! I'm Sarah</h1>
-          <p>I am a software engineer located in South Florida.</p>
+      <Container className="intro-cont">
+        <Row className="intro-row">
+        <Col sm={12} md={6} className="image-col">
+          <Image className="my-image" fluid src='/images/skFront.jpg' />
+        </Col>
+        <Col className="text-col">
+          <h1>Hi! I'm Sarah Kuss</h1>
+          <p>I am a software engineer located in South Florida. </p>
         </Col>
         </Row>
       </Container>
