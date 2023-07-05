51be306ddd97f877d05121c872be143fefaaf844
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-04 22:42:41 -0400

    started about and working on mapping through portfolio data

---
+++ src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function About () {
+  return (
+    <>
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,7 @@
 import { Card, Container, Row } from "react-bootstrap";
+import portfolio from '../../data/portfolio.json'
 
-export default function Projects () {
+export default function Projects ({data: {title, content, website, cover}}) {
   return(
     <>
     <Container>
