8b9b73d2ed4c6895811f63578f51c2e8465281a9
Sarah Kuss <sarah.kuss33@gmail.com>
2023-07-11 22:21:02 -0400

    fixed typo

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,7 +12,7 @@ export default function Intro () {
           <h1>Hi! I'm Sarah Kuss</h1>
           <p>I am a Full Stack Software Engineer located in South Florida. </p>
           <p>
-            Always learning, I strive to stay up-to-date on the latest technological trends and advancements. I am currently intering as a Web3 Software Engineer, working on a women-led NFT that raises money for scholarships that go to women pursuing tech. 
+            Always learning, I strive to stay up-to-date on the latest technological trends and advancements. I am currently interning as a Web3 Software Engineer, working on a women-led NFT that raises money for scholarships that go to women pursuing tech.   
           </p>
           <p>
             Technology advances every single day and there is always something new to learn. <br /> 
